91c6040c724c1b495295de6fc417284d32ffdf18
Chris Adams <chris@improbable.org>
2009-11-08 13:38:57 -0500

    Archive templatetag improvements
    * renamed get_post_archive to more correct get_post_archive_months
    * added get_post_archive_years

--- basic/blog/templatetags/archive.py
+++ basic/blog/templatetags/archive.py
@@ -5,17 +5,19 @@ from basic.blog.models import Post
 register = template.Library()
 
 class PostArchive(template.Node):
-    def __init__(self, var_name):
+    def __init__(self, var_name, scale="month"):
         self.var_name = var_name
+        self.scale = scale
 
     def render(self, context):
-        dates = Post.objects.published().dates('publish', 'month', order='DESC')
+        dates = Post.objects.published().dates('publish', self.scale, order='DESC')
         if dates:
             context[self.var_name] = dates
         return ''
 
 @register.tag
-def get_post_archive(parser, token):
+def get_post_archive_months(parser, token):
+    """Populates the current context with a list of the unique months in the archive"""
     try:
         tag_name, arg = token.contents.split(None, 1)
     except ValueError:
@@ -25,4 +27,19 @@ def get_post_archive(parser, token):
         raise template.TemplateSyntaxError, "%s tag had invalid arguments" % tag_name
     var_name = m.groups()[0]
     return PostArchive(var_name)
+
+get_post_archive = get_post_archive_months
+
+@register.tag
+def get_post_archive_years(parser, token):
+    """Populates the current context with a list of the unique years in the archive"""
+    try:
+        tag_name, arg = token.contents.split(None, 1)
+    except ValueError:
+        raise template.TemplateSyntaxError, "%s tag requires arguments" % token.contents.split()[0]
+    m = re.search(r'as (\w+)', arg)
+    if not m:
+        raise template.TemplateSyntaxError, "%s tag had invalid arguments" % tag_name
+    var_name = m.groups()[0]
+    return PostArchive(var_name, scale="year")
 
